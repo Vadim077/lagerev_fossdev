3fc6a03539629ca6306a7bbf06e54314d156410e
Vadim077 <lagvadim@gmail.com>
2026-03-05 13:17:32 +0800

    Show test-clusters principe

--- testing/principle/setup.py
+++ testing/principle/setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name="math_demo",
     version="0.0.0",
-    package_dir={"","src"},
+    package_dir={"":"src"},
     packages=find_packages(where="src")
 )
--- testing/principle/tests/test_principles.py
+++ testing/principle/tests/test_principles.py
@@ -1,11 +1,47 @@
 #TODO make it with 'pip instal -e'
 #in project root_dir after setup.py defined
 
-from math_demo import add
+from math_demo import (
+    add,
+    add_with_bug
+)
 
 def test_addition():
-    assert 2 + 2 = 4
+    assert add(2, 2) == 4
     print("Test ADDITION PASSED")
 
-if __name__=="-_main__":
+def test_addition_with_bug():
+    assert add_with_bug(2, 2) == 4
+    assert add_with_bug(0, 0) == 0
+    print("Test BUGGED ADDITION PASSED")
+
+def test_addition_duplicate():
+    assert add(6, 7) == 6 + 7
+    print("Test DUPLICATE ADDITION PASSED")
+
+def test_addition_overkill():
+    for i in range(0,2**32):
+        for j in range(0,2**64):
+            assert add(i,j) == i + j #wiolatiom pf duplication
+            assert add(-i,j) == -i + j
+            assert add(-i,-j) == -i - j
+            assert add(i,-j) == i - j
+
+def test_addition_ckassters():
+    assert add(7,6)==13
+    assert add(0,6)==6
+    assert add(7,0)==7
+    assert add(10,-11)==-1
+    assert add(-5,0)==-5
+    assert add(0,-2)==-2
+    assert add(9,5)==14
+    assert add(5,9)==14
+    print("Test CLUSTERS PASSED")
+
+
+
+if __name__=="__main__":
     test_addition()
+    test_addition_with_bug()
+    test_addition_duplicate()
+    #test_addition_overkill()
