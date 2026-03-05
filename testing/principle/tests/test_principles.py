8dd47e407cc9030777c6a57cd7a4cd0956ef78e3
Vadim077 <lagvadim@gmail.com>
2026-03-05 13:22:16 +0800

    Added new principle: test logical

--- testing/principle/tests/test_principles.py
+++ testing/principle/tests/test_principles.py
@@ -38,10 +38,14 @@ def test_addition_ckassters():
     assert add(5,9)==14
     print("Test CLUSTERS PASSED")
 
-
+def test_addition_commutative():
+    assert add(9,5)==14
+    assert add(5,9)==14
+    print("Test COMMUTATIVE PASSED")
 
 if __name__=="__main__":
     test_addition()
     test_addition_with_bug()
     test_addition_duplicate()
     #test_addition_overkill()
+    test_addition_commutative()
