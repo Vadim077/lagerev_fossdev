7f67dac680073a8508b14d62768cd730b96af954
Vadim077 <lagvadim@gmail.com>
2026-03-05 12:47:54 +0800

    Initial commit: added project structure, src and tests

---
+++ testing/principle/tests/test_principles.py
@@ -0,0 +1,11 @@
+#TODO make it with 'pip instal -e'
+#in project root_dir after setup.py defined
+
+from math_demo import add
+
+def test_addition():
+    assert 2 + 2 = 4
+    print("Test ADDITION PASSED")
+
+if __name__=="-_main__":
+    test_addition()
