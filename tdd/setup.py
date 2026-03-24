34573b12730a5484eb98425b0e82ccc67a26470f
Vadim077 <93854918+Vadim077@users.noreply.github.com>
2026-03-24 21:13:52 +0800

    Update setup.py

--- tdd/setup.py
+++ tdd/setup.py
@@ -1,8 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="ndfl",
-    version="0.0.0",
+    name="ndfl-calculator-pypi-firstl",
+    version="0.1.0",
+    long_description="Repository: https://github.com/Vadim077/lagerev_fossdev",
+    package_dir={"": "src"},
+    url="https://github.com/Vadim077/lagerev_fossdev",
     package_dir={"": "src"},
     packages=find_packages(where="src")
 )
