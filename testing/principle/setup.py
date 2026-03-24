9de6ce2eb52f9067ffe2f28948084458b6f30f40
Vadim077 <93854918+Vadim077@users.noreply.github.com>
2026-03-24 21:04:02 +0800

    Update setup.py

--- testing/principle/setup.py
+++ testing/principle/setup.py
@@ -1,8 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="math_demo",
+    name="firstt-pypi_math_demo",
     version="0.0.0",
     package_dir={"":"src"},
+    url="https://github.com/Vadim077/lagerev_fossdev",
+    package_dir={"": "tdd/src"},
     packages=find_packages(where="src")
 )
